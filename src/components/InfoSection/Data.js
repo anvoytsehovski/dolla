8c4953fc96b2afb96144753a16fea9fa61c5cc76
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2021-04-27 16:00:52 +0300

    Adding Data to Info Section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDest: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to out exclusive app thaat allows you to send unlimited transactions without getting charget any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
