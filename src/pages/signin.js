ca9ad4d42fcab04c5a228ffe0141df7abf0573f4
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2021-04-28 16:30:39 +0300

    Creating Sign In Component

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import SignIn from '../components/Signin'
 
 const SigninPage = () => {
     return (
-        <div>
-            <h1>Sign is page</h1>
-        </div>
+        <>
+            <SignIn />
+        </>
     )
 }
 
