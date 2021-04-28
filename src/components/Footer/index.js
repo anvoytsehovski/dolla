c35b06280c634cd3bc80c513beb1bae792d76dc9
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2021-04-28 10:01:42 +0300

    Adding React Scroll to our site

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { animateScroll as scroll } from 'react-scroll'
 import {
     FaFacebook,
     FaInstagram,
@@ -23,6 +24,10 @@ import {
 } from './FooterElements'
 
 const Footer = () => {
+    const toggleHome = () => {
+        scroll.scrollToTop()
+    }
+
     return (
         <FooterContainer>
             <FooterWrap>
@@ -63,7 +68,7 @@ const Footer = () => {
                 </FooterLinksContainer>
                 <SocialMedia>
                     <SocialMediaWrap>
-                        <SocialLogo to='/'>dolla</SocialLogo>
+                        <SocialLogo to='/' onClick={toggleHome}>dolla</SocialLogo>
                         <WebsiteRights>&copy; {new Date().getFullYear()} All rights reserved.</WebsiteRights>
                         <SocialIcons>
                             <SocialIconLink href="/" target="_blank" aria-label="Facebook">
